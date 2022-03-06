d163ee56edd1647a483f0b39101fe13425b7c751
KanishkarU <kanishkar1705@gmail.com>
2022-03-06 16:50:49 +0530

    kanishkar

---
+++ src/org/bike/Ktm.java
@@ -0,0 +1,24 @@
+package org.bike;
+
+public class Ktm implements Bike {
+
+	@Override
+	public void cost() {
+		System.out.println("2lakhs");
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void speed() {
+		System.out.println("200kmph");
+		// TODO Auto-generated method stub
+		cost();
+		
+	}
+	public static void main(String[] args) {
+		Ktm a = new Ktm();
+		a.speed();
+	}
+
+}
